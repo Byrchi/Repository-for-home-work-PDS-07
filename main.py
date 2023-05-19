60f7031267ee86ba830f80d980af9f82701a2da6
OLeh Berehulia <iuad1a1k@gmail.com>
2023-05-19 16:29:29 +0200

    002 "master" Add new function for mane.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def print_hi(name):
        print(f'Hi, {name}')
-
+def mane_function():
+    for i in range(0,100):
+        if i % 13 == 0:
+            return i
 
 
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(mane_function())
 
 

9e2f64db3f54f6f8e5e9ace223dd934face2fcad
OLeh Berehulia <iuad1a1k@gmail.com>
2023-05-19 19:27:53 +0200

    011 "test_py_charm" Add new function to py_charm.py

--- py_charm.py
+++ py_charm.py
@@ -1,4 +1,31 @@
+
+
+
+
 print('Hello world!')
+
+
+
+def binary_search(lst, val):
+    first = 0
+    last = len(lst) - 1
+    index = -1
+    while (first <= last) and (index== -1):
+        mid = (first + last) // 2
+        if lst[mid] == val:
+            index = mid
+        else:
+            if val < lst[mid]:
+                last = mid-1
+            else:
+                first = mid +1
+    return index
+
+
+l = list(range(0, 101))
+# l = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# l = [70, 30, 20, 70, 100, 60, 400, 20, 30, 100]
+print(binary_search(l, 48))
 
 
 import time
@@ -22,3 +49,6 @@ val = 5628
 print(InterpolationSearch(lys, val))
 end = time.time()
 print('Час виконання даної функції', end-start, 'c')
+
+
+

a6525d5ce3b589edc123cf92b923c3619d0872e9
Oleh Berehulia <iuad1a1k@gmail.com>
2023-06-08 23:22:21 +0200

    002

--- dz_13/dz_13_task_01.py
+++ dz_13/dz_13_task_01.py
@@ -1,5 +1,6 @@
 import random
 from random_words import RandomWords
+import time
 
 
 int_list = []
@@ -12,6 +13,50 @@ for i in range(0, 5000):
     str_list.append(word.random_word())
 
 
-print(int_list)
-print(float_list)
-print(str_list)
+# print(int_list)
+# print(float_list)
+# print(str_list)
+
+##### Task_02 #####
+def bubble_sort(data):
+    lenght = len(data)
+    for iIndex in range(lenght):
+        swapped = False
+        for jIndeex in range(0, lenght - iIndex -1):
+            if data [jIndeex] > data[jIndeex + 1]:
+                data[jIndeex], data[jIndeex + 1] =  data[jIndeex + 1], data[jIndeex]
+                swapped = True
+        if not swapped:
+            break
+    # print('Sorted data', data)
+
+
+cor_time = time.time()
+bubble_sort(int_list)
+print('*'*50)
+print(f'Duration time: {time.time() - cor_time}')
+
+cor_time = time.time()
+bubble_sort(float_list)
+print('*'*50)
+print(f'Duration time: {time.time() - cor_time}')
+
+cor_time = time.time()
+bubble_sort(str_list)
+print('*'*50)
+print(f'Duration time: {time.time() - cor_time}')
+
+
+def average_time(arr, q_iteration):
+    results_sum = 0
+    for i in range(q_iteration):
+        cor_time = time.time()
+        bubble_sort(arr)
+        res_time = time.time() - cor_time
+        results_sum  += res_time
+    result = results_sum/q_iteration
+    return print(result)
+print('*'*50)
+average_time(int_list, 10)
+average_time(float_list, 10)
+average_time(str_list, 10)

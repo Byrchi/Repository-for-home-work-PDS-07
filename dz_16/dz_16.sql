e2c1a0d2e9d1db60bbcc9a5c62049fc1d43daaec
Oleh Berehulia <iuad1a1k@gmail.com>
2023-06-16 15:39:11 +0200

    dz_16

---
+++ dz_16/dz_16.sql
@@ -0,0 +1,5 @@
+SELECT * FROM employees  ORDER BY FIRST_NAME ASC;
+-- SELECT concat(FIRST_NAME," ",LAST_NAME) AS name, SALARY, SALARY * 0.15 AS TAX FROM employees;
+-- SELECT SUM(SALARY) FROM employees ;
+-- SELECT min(SALARY), max(SALARY)  FROM employees ;
+-- SELECT avg(SALARY) , count(EMPLOYEE_ID) FROM employees;
